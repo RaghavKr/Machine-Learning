deb0690833a9d8f8ce9f495088d03c21a9b1b362
RaghavKr <49243362+RaghavKr@users.noreply.github.com>
2019-08-12 21:10:30 -0400

    Update decision_tree_regression.py

--- UdemyCourse/decision_tree_regression.py
+++ UdemyCourse/decision_tree_regression.py
@@ -31,14 +31,6 @@ regressor.fit(X,y)
 
 y_pred = regressor.predict([[6.5]])
 
-# Visualising the Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
 # Visualising the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
